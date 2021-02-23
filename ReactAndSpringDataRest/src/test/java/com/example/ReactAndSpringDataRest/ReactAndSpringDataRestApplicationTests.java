1928abbd283b6f38ea37c7648c20d012bfccfb24
Oleksandr Bieliakov <abel9k@gmail.com>
2021-02-23 17:03:29 +0100

    React and Sprind Data REST tutorial initial commit

---
+++ ReactAndSpringDataRest/src/test/java/com/example/ReactAndSpringDataRest/ReactAndSpringDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReactAndSpringDataRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactAndSpringDataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
