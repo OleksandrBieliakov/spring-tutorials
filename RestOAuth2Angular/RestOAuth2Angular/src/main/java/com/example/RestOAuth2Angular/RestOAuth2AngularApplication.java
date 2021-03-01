97b235dd11ad2fa6f20f78c17d4ada984b3a0cdc
Oleksandr Bieliakov <abel9k@gmail.com>
2021-03-01 14:18:11 +0100

    Tutorial REST API with OAuth2 and Angular: API initial commit

---
+++ RestOAuth2Angular/RestOAuth2Angular/src/main/java/com/example/RestOAuth2Angular/RestOAuth2AngularApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestOAuth2Angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestOAuth2AngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestOAuth2AngularApplication.class, args);
+	}
+
+}
